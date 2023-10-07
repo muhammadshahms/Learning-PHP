ac9250ab7b111ea830d1ecd120686f8357c98cdf
muhammadshahms <muhammadshah516@gmail.com>
2023-10-07 18:48:06 +0500

    .

--- php_project/index.php
+++ php_project/index.php
@@ -7,25 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
-<body>
-    <h1>
-        Form
-    </h1>
-    <form action="" method="post" enctype="multipart/form-data">
-        <input type="text" name="name" id="name">
-        <br>
-        <input type="email" name="email" id="email">
-        <br>
-        <input type="password" name="password" id="password">
-        <br>
-        <input type="file" name="file" id="file" >
-        <br>
-        <button type="submit" name="btn_saved">Save</button>
-    </form>
-</body>
-</html>
-
 <?php
 include('connection.php');
 if (isset($_POST["btn_saved"])) {
@@ -41,12 +22,33 @@ if (isset($_POST["btn_saved"])) {
         move_uploaded_file($_FILES["file"]["tmp_name"], $destinationPath);
         echo "<script>alert('user saved')</script>";
         echo "<script>window.location.assign('users.php')</script>";
+        
         // header("location:login.php");
     } else {
         echo "<script>alert('user not saved')</script>";
     }
 }
 ?>
+<body>
+    <h1>
+        Form
+    </h1>
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="text" name="name" id="name">
+        <br>
+        <input type="email" name="email" id="email">
+        <br>
+        <input type="password" name="password" id="password">
+        <br>
+        <input type="file" name="file" id="file">
+        <?php //echo "<img src='$destinationPath' alt='Uploaded Image'>"; ?>
+        <br>
+        <button type="submit" name="btn_saved">Save</button>
+    </form>
+</body>
+</html>
+
+
 <!-- 
 
 $originalFilename = $_FILES["file"]["name"];
