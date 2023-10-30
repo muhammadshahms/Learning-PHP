987e2c73c58b23476f09d34210dd228b3cbbfd57
muhammadshahms <muhammadshah516@gmail.com>
2023-10-30 18:00:37 +0500

    .

--- 2212F1/user_insert.php
+++ 2212F1/user_insert.php
@@ -1,29 +1,27 @@
 <?php
 require "connection.php";
 require "./templates/header.php";
-
 ?>
-
 
 <form method="POST">
     <div class="container">
         <div class="mb-3">
             <label for="" class="form-label">Name</label>
             <input type="text" class="form-control" name="name" id="" aria-describedby="nameHelpId" placeholder="Enter your name" required>
-            <small id="emailHelpId" class="form-text text-muted"></small>
+
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Email</label>
             <input type="email" class="form-control" name="email" id="" aria-describedby="emailHelpId" placeholder="abc@mail.com" required>
-            <small id="emailHelpId" class="form-text text-muted"></small>
+
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Password</label>
             <input type="password" class="form-control" name="password" id="" aria-describedby="passwordHelpId" placeholder="Create your Password" required>
-            <small id="emailHelpId" class="form-text text-muted"></small>
+
         </div>
 
-        <button type="submit" class="btn btn-primary" name="submit" >Submit</button>
+        <button type="submit" class="btn btn-primary" name="submit">Submit</button>
     </div>
 
 </form>
@@ -31,14 +29,13 @@ require "./templates/header.php";
 
 <?php
 require "./templates/footer.php";
-if(isset($_POST["submit"])){
-    $query = "INSERT INTO `users`(`name`, `email`, `password`) VALUES ('". $_POST["name"] ."','". $_POST["email"] ."','". md5($_POST["password"]) ."')";
+if (isset($_POST["submit"])) {
+    $query = "INSERT INTO `users`(`name`, `email`, `password`) VALUES ('" . $_POST["name"] . "','" . $_POST["email"] . "','" . md5($_POST["password"]) . "')";
     $result = mysqli_query($con, $query);
-    if($result){
+    if ($result) {
         echo "<script>alert('inserted')</script>";
         echo "<script>window.location.href='user_table.php'</script>";
-    }
-    else{
+    } else {
         echo "not inserted";
     }
 }
