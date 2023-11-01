2b93b56964765b8824611af77da987fa1ee3cd31
Shah <muhammadshah516@gmail.com>
2023-11-01 12:26:10 +0500

    .

--- php_project/connection.php
+++ php_project/connection.php
@@ -5,6 +5,20 @@ $dbusername = "root";
 $dbpassword = "";
 $dbname = "db_php";
 
+try {
+    // Create a database connection
+    $con = mysqli_connect($domain, $dbusername, $dbpassword, $dbname);
 
-    $connect = mysqli_connect($domain, $dbusername, $dbpassword, $dbname);
+    // Check if the connection was successful
+    if (!$con) {
+        throw new Exception("Connection failed: " . mysqli_connect_error());
+    }
+
+    // Your database operations go here
+
+} catch (Exception $e) {
+    // Handle the exception
+    echo "Database Error: " . $e->getMessage();
+}
+
 ?>
