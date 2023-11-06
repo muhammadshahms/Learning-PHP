80b78a959cb2b5462ceb79f38a9d8d8c9d8c5f40
Shah <muhammadshah516@gmail.com>
2023-11-06 11:53:28 +0500

    .

--- 2212F1/user_login.php
+++ 2212F1/user_login.php
@@ -2,11 +2,35 @@
 require 'connection.php';
 include './templates/Bootstrap/cdn.php';
 session_start();
+
+if (isset($_POST["btn_login"])) {
+            $query = "SELECT * FROM users WHERE email = '" . $_POST["email"] . "' AND password = '" . md5($_POST["password"]) . "'";
+            $result = mysqli_query($con, $query);
+            if (mysqli_num_rows($result) > 0) {
+                if ($session = mysqli_fetch_array($result)) {
+                    $_SESSION["email"] = $session["email"];
+                    $_SESSION["id"] = $session["id"];
+                    $_SESSION["name"] = $session["name"];
+                    $_SESSION["role"] = $session["role"];
+                    header("Location: user_table.php");
+                    exit();
+                }
+            } else {
+                header("Location: user_login.php?error=Invalid login credentials");
+                exit();
+            }
+        }
+    
+
 ?>
-<div 
-    class="container d-flex justify-content-center align-items-center" 
-    style="min-height: 100vh">
+<div class="container d-flex flex-column justify-content-center align-items-center" style="min-height: 100vh">
+    <h1 class="display-3">Login</h1>
     <form class="border shadow p-3 rounded" method="post" style="width: 450px;">
+        <?php if (isset($_GET['error'])) { ?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo $_GET['error']; ?>
+            </div>
+        <?php } ?>
         <div class="mb-3">
             <label for="" class="form-label">Email</label>
             <input type="text" class="form-control" name="email" id="email" aria-describedby="helpId" placeholder="Enter your email">
@@ -18,21 +42,3 @@ session_start();
         <input class="btn btn-success" type="submit" value="Login" name="btn_login">
     </form>
 </div>
-<?php
-
-if (isset($_POST["btn_login"])) {
-    $query = "select * from users where email = '" . $_POST["email"] . "' and password = '" . md5($_POST["password"]) . "'";
-    $result = mysqli_query($con, $query);
-    if (mysqli_num_rows($result) > 0) {
-        if ($q = mysqli_fetch_array($result)) {
-            // $_SESSION["email"] = $q["email"];
-            $_SESSION["id"] = $q["id"];
-            // $_SESSION["name"] = $q["name"];   
-            echo "<script>window.location.assign('user_table.php')</script>";
-        }
-    } else {
-        echo "<script>alert('Login Failed')</script>";
-    }
-}
-
-?>
