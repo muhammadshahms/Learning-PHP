b9ae9e3d2231d4906bb7bb3db09be0b4c3074fe9
Shah <muhammadshah516@gmail.com>
2023-11-08 15:12:29 +0500

    .

--- 2212F1/middleware/auth_middleware.php
+++ 2212F1/middleware/auth_middleware.php
@@ -1,15 +1,14 @@
 <?php
 session_start();
 function userMiddlewareAuth() {
-    // Check if the user is not logged in or the role is not 'user'
-    if (!isset($_SESSION["id"]) || $_SESSION["role"] !== "user") {
+    if (!(isset($_SESSION["id"]) || $_SESSION["role"] === "user")) {
         header("Location: login.php");
         exit();
     }
 }
 
 function adminMiddlewareAuth() {
-    if (!(isset($_SESSION["id"]) && $_SESSION["role"] === "admin")) {
+    if (!(isset($_SESSION["id"]) || $_SESSION["role"] === "admin")) {
         header("Location: ../login.php");
         exit();
     }
