53e2d79c4f7bac0972fa974f6df7e23fcb05dbd1
muhammadshahms <muhammadshah516@gmail.com>
2023-11-11 20:11:21 +0500

    .

---
+++ OOP/class.php
@@ -0,0 +1,21 @@
+<?php
+
+class Fruits {
+    public $name;
+    public $color;
+
+    public function __construct($name, $color) {
+        $this->name = $name;
+        $this->color = $color;
+    }
+
+    public function intro() {
+        echo "The fruit is {$this->name} and the color is {$this->color}.";
+    }
+
+
+}
+
+$b = new Fruits("Apple", "red");
+
+$b->intro();
